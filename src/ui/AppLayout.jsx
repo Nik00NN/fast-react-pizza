2919c37fb4b1817dc16fb4e27c5146e2fdcd3541
Nik00NN <birziloiu.mihaialexandru@outlook.com>
2024-07-30 19:09:51 +0300

    fetching an external api using react-router for Menu component and loaded the data using loader

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import LoadingIndicator from "./LoadingIndicator";
+const AppLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <LoadingIndicator />}
+
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
